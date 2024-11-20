3bb447b6c25a7d3f6875402fc570eb9c9008e7eb
HsiaYuChih <hsiayuchih@outlook.com>
2024-11-20 10:18:23 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '珠珠',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'f6f759253696438385b7db8c17efbd05',
+      id: 'SCT263308TTJlJXaeZC5lhdvSgGvByn6FA',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: '0001',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
@@ -57,7 +57,7 @@ const USER_CONFIG = {
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '4a29ef4628a947a3ba3923bfc5bcb9c9',
+      id: 'SCT263308TTJlJXaeZC5lhdvSgGvByn6FA',
     }
   ],
 
